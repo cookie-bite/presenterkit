f76e1d6796993472c74364d417526d4d33971030
Ali Mammadli <nfsavss@icloud.com>
2023-02-12 03:56:40 +0400

    Fixed text color light and saturation

--- client/src/components/core.sc.js
+++ client/src/components/core.sc.js
@@ -63,7 +63,8 @@ export const Scene = () => {
         console.log('got reply!', JSON.parse(data).message)
         const sentence = JSON.parse(data).message
         let tempColor = `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
-        const color = tempColor.length === 7 ? tempColor : `${tempColor}f`
+        const colorT = tempColor.length === 7 ? tempColor : `${tempColor}f`
+        const color = hslToHex(hexToHsl(colorT).h, hexToHsl(colorT).s  < 30 ? 100 - hexToHsl(colorT).s : hexToHsl(colorT).s, hexToHsl(colorT).l < 50 ? 100 - hexToHsl(colorT).l : hexToHsl(colorT).l)
 
 
         setTexts([...texts, {
@@ -122,6 +123,45 @@ export const Scene = () => {
                 ],
             color: `#${colorsArray[Math.floor(Math.random() * colorsArray.length)]}`
         }])
+    }
+
+    const hexToHsl = (hex) => {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+        var r = parseInt(result[1], 16)
+        var g = parseInt(result[2], 16)
+        var b = parseInt(result[3], 16)
+        r /= 255, g /= 255, b /= 255
+        var max = Math.max(r, g, b), min = Math.min(r, g, b)
+        var h, s, l = (max + min) / 2
+        if (max == min) {
+            h = s = 0 // achromatic
+        } else {
+            var d = max - min
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
+            switch (max) {
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6
+        }
+
+        h = Math.round(h * 360)
+        s = Math.round(s * 100)
+        l = Math.round(l * 100)
+
+        return { h, s, l }
+    }
+
+    const hslToHex = (h, s, l) => {
+        l /= 100
+        const a = s * Math.min(l, 1 - l) / 100
+        const f = n => {
+            const k = (n + h / 30) % 12
+            const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+            return Math.round(255 * color).toString(16).padStart(2, '0')   // convert to Hex and prefix "0" if needed
+        }
+        return `#${f(0)}${f(8)}${f(4)}`
     }
 
 
@@ -236,5 +276,5 @@ export const Scene = () => {
             <Light />
             <Text />
         </>
-    );
+    )
 }
