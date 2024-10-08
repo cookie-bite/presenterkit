998a8754352d263228539f67838fc69c65f45bcc
Ali Mammadli <nfsavss@icloud.com>
2024-10-08 10:06:59 +0400

    Made dashboard header fixed

--- client/src/interface/dashboard/events.board.jsx
+++ client/src/interface/dashboard/events.board.jsx
@@ -50,35 +50,40 @@ export const Events = () => {
 
 
     return (
-        <div className={sty.eventsPage}>
+        <div className={sty.page}>
             <div className={sty.header}>
                 <h1>Events</h1>
             </div>
-            <div className={sty.eventForm}>
-                <input className={sty.eventInput} name='eventName' placeholder='Event name' type='text' value={eventName} onChange={(e) => onChange(e.target.value, setEventName)} />
-                <button className={sty.eventBtn} onClick={() => create()}>Create</button>
-            </div>
 
-            <div className={sty.events}>
-                {SSUser.events.map((event) => {
-                    return (
-                        <div className={sty.event} key={event.eventID}>
-                            <div className={sty.eventDetails}>
-                                <h1 className={sty.eventName}>{event.name}</h1>
-                                <h3 className={sty.eventCodeLbl}>Code: <strong className={sty.eventCode}>{event.eventID}</strong></h3>
-                            </div>
-                            <div className={sty.eventBtns}>
-                                <button className={sty.deleteBtn} onClick={() => deleteBy(event.eventID)}>
-                                    <Icon name='trash-o' size={24} color='--red' />
-                                </button>
-                                <button className={sty.joinBtn} onClick={() => joinEvent(event.eventID)}>
-                                    <Icon name='open-o' size={24} color='--white' style={{ marginRight: 5 }} />Start
-                                </button>
-                            </div>
-                            <br />
-                        </div>
-                    )
-                })}
+            <div className={sty.pageInner}>
+                <div className={sty.eventsPage}>
+                    <div className={sty.eventForm}>
+                        <input className={sty.eventInput} name='eventName' placeholder='Event name' type='text' value={eventName} onChange={(e) => onChange(e.target.value, setEventName)} />
+                        <button className={sty.eventBtn} onClick={() => create()}>Create</button>
+                    </div>
+
+                    <div className={sty.events}>
+                        {SSUser.events.map((event) => {
+                            return (
+                                <div className={sty.event} key={event.eventID}>
+                                    <div className={sty.eventDetails}>
+                                        <h1 className={sty.eventName}>{event.name}</h1>
+                                        <h3 className={sty.eventCodeLbl}>Code: <strong className={sty.eventCode}>{event.eventID}</strong></h3>
+                                    </div>
+                                    <div className={sty.eventBtns}>
+                                        <button className={sty.deleteBtn} onClick={() => deleteBy(event.eventID)}>
+                                            <Icon name='trash-o' size={24} color='--red' />
+                                        </button>
+                                        <button className={sty.joinBtn} onClick={() => joinEvent(event.eventID)}>
+                                            <Icon name='open-o' size={24} color='--white' style={{ marginRight: 5 }} />Start
+                                        </button>
+                                    </div>
+                                    <br />
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     )
--- client/src/interface/dashboard/payments.board.jsx
+++ client/src/interface/dashboard/payments.board.jsx
@@ -5,9 +5,14 @@ import sty from '../../styles/modules/dashboard.module.css'
 export const Payments = () => {
 
     return (
-        <div className={sty.payments}>
+        <div className={sty.page}>
             <div className={sty.header}>
                 <h1>Payments</h1>
+            </div>
+            <div className={sty.pageInner}>
+                <div className={sty.payments}>
+                    
+                </div>
             </div>
         </div>
     )
