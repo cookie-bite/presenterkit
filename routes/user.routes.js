92650ab301d6afe5157b23ca614e44bb249effd0
Ali Mammadli <nfsavss@icloud.com>
2025-07-13 22:13:07 +0400

    fix: Exclude sensitive fields from user data response

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,7 +11,7 @@ module.exports = router
 
 router.get('/data', authUser, async (req, res) => {
   try {
-    const user = await collection('users').findOne({ _id: req.user._id }, { projection: { _id: 0 } })
+    const user = await collection('users').findOne({ _id: req.user._id }, { projection: { _id: 0, password: 0, refreshToken: 0 } })
     res.status(200).json({ success: true, user })
   } catch (err) { res.status(500).json({ success: false, err: err }) }
 })
