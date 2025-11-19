ad048970fb322ae95b5b24943ba4128c4e5aa204
Ali Mammadli <nfsavss@icloud.com>
2025-11-19 22:37:58 +0400

    chore: add eslint-plugin-simple-import-sort for improved import sorting

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
@@ -10,40 +11,43 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
 export default defineConfig([
-    globalIgnores(["**/dist", "**/node_modules", "**/coverage", "**/*.js.map", "**/*.d.ts"]),
-    {
-        extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+  globalIgnores(["**/dist", "**/node_modules", "**/coverage", "**/*.js.map", "**/*.d.ts"]),
+  {
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
 
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
-
-            parser: tsParser,
-            ecmaVersion: 2023,
-            sourceType: "module",
-
-            parserOptions: {
-                project: "./tsconfig.json",
-            },
-        },
-
-        rules: {
-            "@typescript-eslint/interface-name-prefix": "off",
-            "@typescript-eslint/explicit-function-return-type": "off",
-            "@typescript-eslint/explicit-module-boundary-types": "off",
-            "@typescript-eslint/no-explicit-any": "off",
-        },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      "simple-import-sort": simpleImportSort,
     },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2023,
+      sourceType: "module",
+
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ]);
