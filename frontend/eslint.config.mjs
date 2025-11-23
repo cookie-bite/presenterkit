116f0a4f970f55ed9c61c8840cc0045e8247bf8c
Ali Mammadli <nfsavss@icloud.com>
2025-11-23 19:43:01 +0400

    chore: update ESLint configuration to extend from eslint-config-next and remove deprecated imports

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,20 +3,10 @@ import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import nextVitals from 'eslint-config-next/core-web-vitals';
 
 export default defineConfig([
+  ...nextVitals,
   globalIgnores([
     '**/dist',
     '**/node_modules',
@@ -27,13 +17,12 @@ export default defineConfig([
     '**/build',
     '**/public',
   ]),
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     plugins: {
       '@typescript-eslint': typescriptEslint,
       'simple-import-sort': simpleImportSort,
     },
-
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -52,7 +41,6 @@ export default defineConfig([
         project: './tsconfig.json',
       },
     },
-
     rules: {
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
