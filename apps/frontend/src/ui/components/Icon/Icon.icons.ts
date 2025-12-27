3a841a16ad0d5bb34370749d47daa1dc95c5acdd
Ali Mammadli <nfsavss@icloud.com>
2025-12-27 22:26:38 +0400

    feat: add new folder icons

--- apps/frontend/src/ui/components/Icon/Icon.icons.ts
+++ apps/frontend/src/ui/components/Icon/Icon.icons.ts
@@ -1,3 +1,5 @@
 export { default as add } from './icons/add.svg';
 export { default as 'add-circle-outline' } from './icons/add-circle-outline.svg';
 export { default as 'alert-circle-outline' } from './icons/alert-circle-outline.svg';
+export { default as 'folder-open' } from './icons/folder-open.svg';
+export { default as 'folder-open-outline' } from './icons/folder-open-outline.svg';
